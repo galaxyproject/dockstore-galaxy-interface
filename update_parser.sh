83cd9e84b250a1f745fe8ed37632c923ed7332b0
Denis Yuen <denis.yuen@oicr.on.ca>
2022-01-25 16:33:57 -0500

    Updaate to 0.15.0 of gxformat2

--- update_parser.sh
+++ update_parser.sh
@@ -2,6 +2,7 @@
 
 # Until we're publishing gxformat2 to Maven Central just copy from
 # gxformat2 source (assumed to be under parent directory).
+# gxformat2 is cloned from https://github.com/galaxyproject/gxformat2
 package="org/galaxyproject/gxformat2"
 
 for src_type in "main/java" "test/java" "test/resources"
@@ -13,3 +14,8 @@ done
 # Depends on files not copied over yet...
 rm "src/test/java/${package}/LintExamplesTest.java"
 rm "src/test/java/${package}/CytoscapeExamplesTest.java"
+
+# Denis: restore modified files that are not present/modified in gxformat2
+git checkout src/main/java/org/galaxyproject/gxformat2/Cytoscape.java
+git restore src/main/java/org/galaxyproject/gxformat2/CytoscapeDAG.java
+git restore src/test/java/org/galaxyproject/gxformat2/CytoscapeTest.java
